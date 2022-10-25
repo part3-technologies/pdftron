90c1f8f45a5d47104af22be712ecb7fe59dda263
Corey Stanford <corey.michael.stanford@gmail.com>
2022-10-25 15:05:07 -0400

    setup basic app

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import envCompatible from 'vite-plugin-env-compatible';
+import svgr from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+	envPrefix: 'REACT_APP_',
+	plugins: [
+		react(),
+		envCompatible,
+		svgr(),
+		eslint({ rulePaths: ['./'] }),
+	],
+	build: {
+		emptyOutDir: true,
+		outDir: 'build',
+	},
+	server: {
+		port: 3000,
+	}
+});
